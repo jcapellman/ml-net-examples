e96465dc5ce0bdc5f60c3312352b9e6761870b7c
Jarred Capellman <jcapellman@hotmail.com>
2019-06-14 21:27:04 -0500

    Added: Initial UWP App

---
+++ UWP-Browser-Classification/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using UWP_Browser_Classification.ViewModels;
+
+namespace UWP_Browser_Classification
+{
+    public sealed partial class MainPage : Page
+    {
+        private MainPageViewModel ViewModel => (MainPageViewModel) DataContext;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DataContext = new MainPageViewModel();
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            var webServiceUrl = ViewModel.WebServiceURL;
+
+            if (!webServiceUrl.StartsWith("http://", StringComparison.InvariantCultureIgnoreCase) &&
+                !webServiceUrl.StartsWith("https://", StringComparison.CurrentCultureIgnoreCase))
+            {
+                webServiceUrl = $"http://{webServiceUrl}";
+            }
+
+            wvMain.Navigate(new Uri(webServiceUrl));
+        }
+    }
+}
---
+++ UWP-Browser-Classification/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;using System.Runtime.CompilerServices;
+
+namespace UWP_Browser_Classification.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private bool _enableGoButton;
+
+        public bool EnableGoButton
+        {
+            get => _enableGoButton;
+
+            set
+            {
+                _enableGoButton = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _webServiceURL;
+
+        public string WebServiceURL
+        {
+            get => _webServiceURL;
+
+            set
+            {
+                _webServiceURL = value;
+
+                OnPropertyChanged();
+
+                EnableGoButton = !string.IsNullOrEmpty(value);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
