846110c33a94ee29eddd9fc84102019c7a7313db
Jarred Capellman <jcapellman@hotmail.com>
2019-06-21 18:45:33 -0500

    Added: FeatureExtract class implementation

--- chapter10/trainer/FeatureExtract.cs
+++ chapter10/trainer/FeatureExtract.cs
@@ -1,10 +1,43 @@
-﻿namespace UWP_Browser_Classification_Trainer
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace UWP_Browser_Classification_Trainer
 {
     public class FeatureExtract
     {
+        private static async Task<string> GetWebContent(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        private static async void GetContentFile(string inputFile, string outputFile)
+        {
+            var urls = File.ReadAllLines(inputFile);
+
+            var urlContent = new List<string>();
+
+            foreach (var url in urls)
+            {
+                var content = await GetWebContent(url);
+
+                urlContent.Add(content);
+            }
+
+            File.WriteAllText(outputFile, string.Join(",", urlContent));
+        }
+
         public static void Extract(string cleanFile, string maliciousFile)
         {
-            
+            GetContentFile(cleanFile, "clean.csv");
+
+            GetContentFile(maliciousFile, "malicious.csv");
         }
     }
 }
