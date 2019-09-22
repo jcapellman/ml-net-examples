36badcb154b4923a6ba7b92b5b51b47cfab6a5f5
Jarred Capellman <jcapellman@hotmail.com>
2019-09-22 13:44:43 -0500

    Added: Chapter 2 Example

---
+++ chapter02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using chapter02.ML;
+
+namespace chapter02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("No arguments passed in, exiting");
+
+                return;
+            }
+
+            switch (args[0])
+            {
+                case "predict":
+                    new Predictor().Predict(args[1]);
+                    break;
+                case "train":
+                    new Trainer().Train(args[1]);
+                    break;
+                default:
+                    Console.WriteLine($"{args[0]} is an invalid option");
+                    break;
+            }
+        }
+    }
+}
