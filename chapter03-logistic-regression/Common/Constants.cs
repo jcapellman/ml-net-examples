0cbf57c50c695d234d0af13c85534219cfab67b0
Jarred Capellman <jcapellman@hotmail.com>
2019-10-01 19:41:05 -0500

    Added: Initial work on the logistic regression example

---
+++ chapter03-logistic-regression/Common/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace chapter03_logistic_regression.Common
+{
+    public class Constants
+    {
+        public const string MODEL_FILENAME = "chapter3.mdl";
+    }
+}
---
+++ chapter03-logistic-regression/ML/Objects/EmploymentHistoryPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace chapter03_logistic_regression.ML.Objects
+{
+    public class EmploymentHistoryPrediction
+    {
+        [ColumnName("Score")]
+        public float DurationInMonths;
+    }
+}
