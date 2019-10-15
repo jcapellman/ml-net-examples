f75c4e078204d4767613b0751322e0ba046c0806
Jarred Capellman <jcapellman@hotmail.com>
2019-10-14 19:09:16 -0500

    Added: Model Evaluation and fixes

--- chapter04/ML/Trainer.cs
+++ chapter04/ML/Trainer.cs
@@ -11,7 +11,7 @@ namespace chapter04.ML
 {
     public class Trainer : BaseML
     {
-        public void Train(string trainingFileName)
+        public void Train(string trainingFileName, string testFileName)
         {
             if (!File.Exists(trainingFileName))
             {
@@ -20,12 +20,17 @@ namespace chapter04.ML
                 return;
             }
 
-            var trainingDataView = MlContext.Data.LoadFromTextFile<CarInventory>(trainingFileName, ',');
+            if (!File.Exists(testFileName))
+            {
+                Console.WriteLine($"Failed to find test data file ({testFileName}");
 
-            var dataSplit = MlContext.Data.TrainTestSplit(trainingDataView, testFraction: 0.4);
+                return;
+            }
 
-            IEstimator<ITransformer> dataProcessPipeline = MlContext.Transforms.Concatenate("Features", 
-                    typeof(CarInventory).ToPropertyList<CarInventory>(nameof(CarInventory.Label)))
+            var trainingDataView = MlContext.Data.LoadFromTextFile<CarInventory>(trainingFileName, ',', hasHeader: false);
+
+            IEstimator<ITransformer> dataProcessPipeline = MlContext.Transforms.Concatenate("Features",
+                typeof(CarInventory).ToPropertyList<CarInventory>(nameof(CarInventory.Label)))
                 .Append(MlContext.Transforms.NormalizeMeanVariance(inputColumnName: "Features",
                     outputColumnName: "FeaturesNormalizedByMeanVar"));
 
@@ -38,21 +43,31 @@ namespace chapter04.ML
 
             var trainingPipeline = dataProcessPipeline.Append(trainer);
 
-            var trainedModel = trainingPipeline.Fit(dataSplit.TrainSet);
-            MlContext.Model.Save(trainedModel, dataSplit.TrainSet.Schema, ModelPath);
+            var trainedModel = trainingPipeline.Fit(trainingDataView);
+            MlContext.Model.Save(trainedModel, trainingDataView.Schema, ModelPath);
 
-            var fccPipeline = trainedModel.Append(MlContext.Transforms
+            var evaluationPipeline = trainedModel.Append(MlContext.Transforms
                 .CalculateFeatureContribution(trainedModel.LastTransformer)
                 .Fit(dataProcessPipeline.Fit(trainingDataView).Transform(trainingDataView)));
 
-            var testSetTransform = trainedModel.Transform(dataSplit.TestSet);
+            var testDataView = MlContext.Data.LoadFromTextFile<CarInventory>(testFileName, ',', hasHeader: false);
 
-          //  var modelMetrics = MlContext.BinaryClassification.Evaluate(data: testSetTransform,
-         //       labelColumnName: nameof(CarInventory.Label),
-         //       scoreColumnName: "Score");
+            var testSetTransform = evaluationPipeline.Transform(testDataView);
 
-        //    Console.WriteLine($"Area Under Curve: {modelMetrics.AreaUnderRocCurve:P2}{Environment.NewLine}" +
-         //                     $"Precision Recall Curve: {modelMetrics.AreaUnderPrecisionRecallCurve:P2}");
+            var modelMetrics = MlContext.BinaryClassification.Evaluate(data: testSetTransform,
+                labelColumnName: nameof(CarInventory.Label),
+                scoreColumnName: "Score");
+
+            Console.WriteLine($"Accuracy: {modelMetrics.Accuracy:P2}");
+            Console.WriteLine($"Area Under Curve: {modelMetrics.AreaUnderRocCurve:P2}");
+            Console.WriteLine($"Area under Precision recall Curve: {modelMetrics.AreaUnderPrecisionRecallCurve:P2}");
+            Console.WriteLine($"F1Score: {modelMetrics.F1Score:P2}");
+            Console.WriteLine($"LogLoss: {modelMetrics.LogLoss:#.##}");
+            Console.WriteLine($"LogLossReduction: {modelMetrics.LogLossReduction:#.##}");
+            Console.WriteLine($"PositivePrecision: {modelMetrics.PositivePrecision:#.##}");
+            Console.WriteLine($"PositiveRecall: {modelMetrics.PositiveRecall:#.##}");
+            Console.WriteLine($"NegativePrecision: {modelMetrics.NegativePrecision:#.##}");
+            Console.WriteLine($"NegativeRecall: {modelMetrics.NegativeRecall:P2}");
         }
     }
 }
--- chapter04/Program.cs
+++ chapter04/Program.cs
@@ -8,12 +8,12 @@ namespace chapter04
     {
         static void Main(string[] args)
         {
-            if (args.Length != 2)
+            if (args.Length < 2)
             {
                 Console.WriteLine($"Invalid arguments passed in, exiting.{Environment.NewLine}{Environment.NewLine}Usage:{Environment.NewLine}" +
                                   $"predict <path to input json file>{Environment.NewLine}" +
                                   $"or {Environment.NewLine}" +
-                                  $"train <path to training data file>{Environment.NewLine}");
+                                  $"train <path to training data file> <path to test data file>{Environment.NewLine}");
 
                 return;
             }
@@ -24,7 +24,7 @@ namespace chapter04
                     new Predictor().Predict(args[1]);
                     break;
                 case "train":
-                    new Trainer().Train(args[1]);
+                    new Trainer().Train(args[1], args[2]);
                     break;
                 default:
                     Console.WriteLine($"{args[0]} is an invalid option");
