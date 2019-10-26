0810a949caa1bb17c9190a3bad4dd4c879bb0805
Jarred Capellman <jcapellman@hotmail.com>
2019-10-25 22:03:48 -0500

    Added: FileData property initialization

--- chapter05/ML/Objects/FileData.cs
+++ chapter05/ML/Objects/FileData.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.ML.Data;
+﻿using System;
+using System.Linq;
+
+using Microsoft.ML.Data;
 
 namespace chapter05.ML.Objects
 {
     public class FileData
     {
-        public FileData(byte[] data)
+        public FileData(Span<byte> data)
         {
+            Size = data.Length;
 
+            Header = Avg(data.Slice(0, (int) (data.Length >= 200 ? 200 : Size)));
         }
+
+        private static float Avg(Span<byte> span) => (float) span.ToArray().Average(a => a);
 
         [LoadColumn(0)] 
         public float Size { get; set; }
