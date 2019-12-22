943f3a038d27080c167dffa3eabb5cabbab3d13e
Jarred Capellman <jcapellman@hotmail.com>
2019-12-22 07:55:28 -0600

    Added: Initial work on the Chapter 8 example

---
+++ chapter08/ML/Base/BaseML.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+using chapter08.Common;
+
+using Microsoft.ML;
+
+namespace chapter08.ML.Base
+{
+    public class BaseML
+    {
+        protected const string FEATURES = "Features";
+
+        protected static string ModelPath => Path.Combine(AppContext.BaseDirectory, Constants.MODEL_FILENAME);
+
+        protected readonly MLContext MlContext;
+
+        protected BaseML()
+        {
+            MlContext = new MLContext(2020);
+        }
+    }
+}
---
+++ chapter08/ML/Objects/MusicPrediction.cs
@@ -0,0 +1,9 @@
+﻿namespace chapter08.ML.Objects
+{
+    public class MusicPrediction
+    {
+        public float Label { get; set; }
+
+        public float Score { get; set; }
+    }
+}
