66af9a36ddf89bdf5ff12a6a7e277ce0022948b6
Jarred Capellman <jcapellman@hotmail.com>
2020-01-05 19:17:43 -0600

    Added: Enum, Helper and Objects

--- chapter09/chapter09.trainer/Program.cs
+++ chapter09/chapter09.trainer/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 
+using chapter09.trainer.Enums;
+using chapter09.trainer.Helpers;
+using chapter09.trainer.Objects;
+
 namespace chapter09.trainer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            
+            Console.Clear();
+
+            var arguments = CommandLineParser.ParseArguments<ProgramArguments>(args);
+
+            switch (arguments.Action)
+            {
+                case ProgramActions.PREDICT:
+                    //new Predictor().Predict(arguments);
+                    break;
+                case ProgramActions.TRAINING:
+                    //new Trainer().Train(arguments);
+                    break;
+                default:
+                    Console.WriteLine($"Unhandled action {arguments.Action}");
+                    break;
+            }
         }
     }
 }
